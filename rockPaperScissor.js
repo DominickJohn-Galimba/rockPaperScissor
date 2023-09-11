b303b9533265a6e2a188ee1a72bfe63b22cbfb8f
Dominickjohn-Galimba <dominickjohn.galimba@gmail.com>
2023-09-11 15:55:28 +0800

    Add the index file change the js file

--- rockPaperScissor.js
+++ rockPaperScissor.js
@@ -37,39 +37,145 @@ function playerRound(playerSelection, computerSelection) {
     }
 
 }
-function game(){
-    let askUser = parseInt(prompt("How many round do you wish to play?: "));
-    let getWin = 0;
-    let getTie = 0;
-    let getLose = 0;
-    for (let i = 0; i < askUser; i++) {
-        const playerSelection = prompt("Choose: Rock, Paper or Scissor");
-        const computerSelection = getComputerChoice();
-        const getWinner = playerRound(playerSelection, computerSelection);
-        console.log(getWinner);
+const gameResult = document.getElementById('result');
+      gameResult.style.marginTop = "1em";
+      gameResult.style.marginBottom = "1em";
+      gameResult.style.textAlign = 'center';
+      gameResult.style.fontFamily = "sans-serif";
+      gameResult.style.fontWeight = "700";
+      gameResult.style.fontSize = "70px"; 
+      gameResult.style.color = '#FF6700';
+const playerCountElement = document.getElementById("win");
+const tieCountElement = document.getElementById("tie");
+const computerCountElement = document.getElementById("lose");
+const textDisplayElement = document.getElementById("textDisplay");
+    textDisplayElement.style.marginTop = ".8em";
+    textDisplayElement.style.textAlign = 'center';
+    textDisplayElement.style.fontFamily = "sans-serif";
+    textDisplayElement.style.fontWeight = "700";
+    textDisplayElement.style.fontSize = "70px"; 
+    textDisplayElement.style.color = '#3B3B58';
+const welcomeElement = document.getElementById("welcome");
+window.onload = () => {
+    welcomeElement.style.display = "block";
+}
+const rockButtonElement = document.getElementById("Rock");
+const paperButtonElement = document.getElementById("Paper");
+const scissorButtonElement = document.getElementById("Scissor");
+
+let playerCount = 0; 
+let tieCount = 0;
+let computerCount = 0;
+
+function displayScore (playerCount, computerCount, tieCount) {
+    playerCountElement.textContent = playerCount;
+    computerCountElement.textContent = computerCount;
+    tieCountElement.textContent = tieCount;
+}
+  
+enableButton();
+
+const rockButton = document.querySelector('#Rock');
+rockButton.addEventListener('click', () => {
+    const playerSelection = "Rock";
+    const computerSelection = getComputerChoice();
+    const getWinner = playerRound(playerSelection, computerSelection);
+    welcomeElement.style.display = 'none';
+    setTimeout(() => {
+        gameResult.textContent = `${getWinner}`;
+        scoreCount(getWinner);
+        displayScore(playerCount, computerCount, tieCount);
+        if (playerCount + computerCount + tieCount === 5) {
+            setTimeout(() => {
+                gameResult.textContent = '';
+                roundWinner(playerCount, computerCount);
+                disableButton()
+            }, 500);
+        }
+    }, 1000)   
+});
+
+
+const paperButton = document.querySelector('#Paper');
+paperButton.addEventListener('click', () => {
+    const playerSelection = "Paper";
+    const computerSelection = getComputerChoice();
+    const getWinner = playerRound(playerSelection, computerSelection);
+    welcomeElement.style.display = 'none';
+    setTimeout(() => {
+        gameResult.textContent = `${getWinner}`;
+        scoreCount(getWinner);
+        displayScore(playerCount, computerCount, tieCount);
+        if (playerCount + computerCount + tieCount === 5) {
+            roundWinner(playerCount, computerCount);
+        }
+    }, 1000)
+});
+
+const scissorButton = document.querySelector("#Scissor");
+scissorButton.addEventListener('click', () => {
+    const playerSelection = "Scissor";
+    const computerSelection = getComputerChoice();
+    const getWinner = playerRound(playerSelection, computerSelection);
+    welcomeElement.style.display = 'none';
+    setTimeout(() => {
+        gameResult.textContent = `${getWinner}`;
+        scoreCount(getWinner);
+        displayScore(playerCount, computerCount, tieCount);
+        if (playerCount + computerCount + tieCount === 5) {
+            roundWinner(playerCount, computerCount);
+        }
+    }, 1000)
+});
+
+const newGameButton = document.querySelector(".newGame");
+newGameButton.addEventListener('click', () => {
+        playerCount = 0;
+        computerCount = 0;
+        tieCount = 0;
+        displayScore(playerCount, computerCount, tieCount);
+        playerCountElement.textContent = "0";
+        computerCountElement.textContent = "0";
+        tieCountElement.textContent = "0";
+        textDisplayElement.textContent = "";
+        gameResult.textContent = ""
+        enableButton();
+})
+
+function scoreCount(getWinner){
+
         if (getWinner === "You Win! Rock beat Scissor!" || getWinner === "You Win! Paper beat Rock!" || getWinner === "You Win! Scissor beat Paper!")  { 
-            getWin += 1;
+            playerCount += 1;
         }
         else if (getWinner === "It's a tie!") {
-            getTie += 1;
+            tieCount += 1;
         }
         else {
-            getLose += 1;
+            computerCount += 1;
         }
     }
-    if (getWin > getLose) {
-        console.log("Congratulations! You Win the Match");
-        console.log (`Win : ${getWin} Loss : ${getLose} Tie : ${getTie}`);
-    }
-    else if (getWin === getLose) {
-        console.log(" It's tie for this match");
-        console.log(`Win : ${getWin} Loss : ${getLose} Tie : ${getTie}`);
-    }
-    else if (getWin < getLose) {
-        console.log(" Unfortunately You Lose");
-        console.log(`Win : ${getWin} Loss : ${getLose} Tie : ${getTie}`);
-    }
-    return "Game Over";
 
+function roundWinner(playerCount, computerCount) {
+    if (playerCount > computerCount) {
+        textDisplayElement.textContent = "Congratulations! You Win the Match";     
+    }
+    else if (playerCount === computerCount) {
+        textDisplayElement.textContent = " It's tie for this match";
+    }
+    else if (playerCount <computerCount) {
+        textDisplayElement.textContent = " Unfortunately You Lose";
+    }
 }
-console.log(game());
+function enableButton (){
+    rockButtonElement.disabled = false;
+    paperButtonElement.disabled = false;
+    scissorButtonElement.disabled = false;
+}
+
+function disableButton (){
+    rockButtonElement.disabled = true;
+    paperButtonElement.disabled = true;
+    scissorButtonElement.disabled = true;
+}
+
+
